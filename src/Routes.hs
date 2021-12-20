cc0aff35e5930a3ea4676d1f1567cacb6f2d4d9e
Joseph Morag <jm@josephmorag.com>
2021-12-19 19:59:14 -0500

    Define servant api type

--- src/Routes.hs
+++ src/Routes.hs
@@ -1,35 +1,33 @@
 {-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
--- |
 module Routes where
 
+import Control.Lens
 import Course
 import Data.Aeson
 import Data.Aeson.Types
 import qualified Data.Text as T
+import Data.Time.Clock
 import Data.Type.Map
+import Data.Type.Set (Sort)
+import Databass
 import GHC.TypeLits
-import Relude hiding (Map)
-import Servant.API
+import Relude hiding (group)
+import Servant
 
 type CourseAPI =
   "courses"
-    :> Get
-        '[JSON]
-        (Map '["courses" ':-> [Map '["id" ':-> Int, "name" ':-> Text]]])
-    :<|> "courses" :> Capture "id" Int
-      :> Get
-          '[JSON]
-          ( Map
-              '[ "id" ':-> Int,
-                 "name" ':-> Text,
-                 "status" ':-> Status,
-                 "created_at" ':-> UTCTime,
-                 "updatedAt" ':-> UTCTime
-               ]
-          )
+    :> ( Get '[JSON] (Tuple '["courses" ::: [Tuple (Course :!! '["id", "name"])]])
+          :<|> Capture "id" Int :> Get '[JSON] (Tuple (Sort (Rename "createdAt" "created_at" (Course :\ "deletedAt"))))
+          :<|> ReqBody '[JSON] (Tuple (Course :!! '["name", "status"]))
+            :> PostCreated '[JSON] (Headers '[Header "Location" Link] (Tuple Course))
+          :<|> Capture "id" Int
+            :> ReqBody '[JSON] (Tuple (Course :!! '["name", "status"]))
+            :> Put '[JSON] (Tuple Course)
+          :<|> Capture "id" Int :> DeleteNoContent
+       )
 
-exGetAllCourses :: Map '["courses" ':-> [Map '["id" ':-> Int, "name" ':-> Text]]]
-exGetAllCourses = Ext Var ([Ext (Var @"id") 123 $ Ext (Var @"name") "Linux for Children" Empty]) Empty
+courseAPI :: Proxy CourseAPI
+courseAPI = Proxy
 
