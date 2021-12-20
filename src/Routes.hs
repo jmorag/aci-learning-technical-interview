aea03a628f4e7db9213fcd7653071f1a40258aa3
Joseph Morag <jm@josephmorag.com>
2021-12-19 23:29:59 -0500

    Move routes to toplevel defs, finish api, fix bugs

--- src/Routes.hs
+++ src/Routes.hs
@@ -13,9 +13,9 @@ import Data.Type.Map
 import Data.Type.Set (Sort)
 import Databass
 import GHC.TypeLits
+import Network.Wai.Handler.Warp
 import Relude hiding (group)
 import Servant
-import Network.Wai.Handler.Warp
 
 type CourseAPI =
   "courses"
@@ -29,12 +29,12 @@ type GetCourse =
 
 type CreateCourse =
   ReqBody '[JSON] (Tuple (Course :!! '["name", "status"]))
-    :> PostCreated '[JSON] (Headers '[Header "Location" Link] (Tuple Course))
+    :> PostCreated '[JSON] (Headers '[Header "Location" Link] (Tuple (Course :\ "deletedAt")))
 
 type UpdateCourse =
   Capture "id" Int
     :> ReqBody '[JSON] (Tuple (Course :!! '["name", "status"]))
-    :> Put '[JSON] (Tuple Course)
+    :> PutNoContent
 
 type DeleteCourse = Capture "id" Int :> DeleteNoContent
 
@@ -42,46 +42,101 @@ courseAPI :: Proxy CourseAPI
 courseAPI = Proxy
 
 type DB = IORef (MapDB Schema, Int)
+
 type InnerMonad = ReaderT DB Handler
 
 server :: ServerT CourseAPI InnerMonad
-server = getCourses :<|> getCourse :<|> createCourse :<|> error "WIP"
-  where
-    getDB = readIORef =<< ask
-    putDB (db', i') = do
-      ref <- ask
-      writeIORef ref (db', i')
-    getCourses = do
-      (db, i) <- getDB
-      let query = table @"courses" @Schema & project @'["id", "name"]
-      pure (Ext Var (runQuery db query) Empty)
-    getCourse id_ = do
-      (db, _) <- getDB
-      let query =
-            table @"courses" @Schema
-              & restrict (\t -> t ^. col @"id" == id_)
-              & rename @"createdAt" @"created_at"
-          results = runQuery db query
-      case results of
-        [] -> throwError $ err404 {errBody = "id " <> show id_ <> " not found"}
-        [r] -> case r ^. col @"deletedAt" of
-          Nothing -> pure (submap r)
-          Just _ -> throwError err410
-        _ ->
-          throwError $
-            err500
-              { errBody = "Internal server error, should only have gotten one result with id " <> show id_
-              }
-    createCourse (Ext _ name (Ext _ status Empty) :: Tuple '["name" ::: _, "status" ::: _]) = do
-      (db, nextId) <- getDB
-      case runQuery db (table @"courses" @Schema & restrict (\t -> t ^. col @"name" == name)) of
-        [] -> do
-          now <- liftIO getCurrentTime
-          let course = nextId <| name <| status <| now <| now <| Nothing <| Empty
-              db' = insert @"courses" @Schema (asMap @Course course) db
-          putDB (db', nextId + 1)
-          pure (addHeader (safeLink courseAPI (Proxy @("courses" :> GetCourse)) nextId) course)
-        _ -> throwError $ err400 { errBody = "Course " <> show (unName name) <> " already exists" }
+server = getCourses :<|> getCourse :<|> createCourse :<|> updateCourse :<|> deleteCourse
+
+getDB = readIORef =<< ask
+
+putDB (db', i') = do
+  ref <- ask
+  writeIORef ref (db', i')
+
+getCourses = do
+  (db, i) <- getDB
+  let query =
+        table @"courses" @Schema
+          & restrict (\t -> t ^. col @"deletedAt" & isNothing)
+          & project @'["id", "name"]
+  pure (Ext (Var @"courses") (runQuery db query) Empty)
+
+getCourse id_ = do
+  (db, _) <- getDB
+  let query =
+        table @"courses" @Schema
+          & restrict (\t -> t ^. col @"id" == id_)
+          & rename @"createdAt" @"created_at"
+      results = runQuery db query
+  case results of
+    [] -> throwError $ err404 {errBody = "Course " <> show id_ <> " not found"}
+    [r] -> case r ^. col @"deletedAt" of
+      Nothing -> pure (submap r)
+      Just _ -> throwError err410
+    _ ->
+      throwError $
+        err500
+          { errBody = "Internal server error, should only have gotten one result with id " <> show id_
+          }
+
+createCourse (Ext _ name (Ext _ status Empty)) = do
+  (db, nextId) <- getDB
+  let query =
+        table @"courses" @Schema
+          & restrict (\t -> (t ^. col @"deletedAt" & isNothing) && t ^. col @"name" == name)
+  case runQuery db query of
+    [] -> do
+      now <- liftIO getCurrentTime
+      let course = nextId <| name <| status <| now <| now <| Nothing <| Empty
+          db' = insert @"courses" @Schema (asMap @Course course) db
+      putDB (db', nextId + 1)
+      pure (addHeader (safeLink courseAPI (Proxy @("courses" :> GetCourse)) nextId) (submap course))
+    _ -> throwError $ err400 {errBody = "Course " <> show (unName name) <> " already exists"}
+
+updateCourse id_ (Ext _ name (Ext _ status Empty)) = do
+  (db, nextId) <- getDB
+  case runQuery db (table @"courses" @Schema & restrict (\t -> t ^. col @"id" == id_)) of
+    [] -> throwError $ err404 {errBody = "Course " <> show id_ <> " not found"}
+    [course]
+      | isJust (course ^. col @"deletedAt") -> throwError $ err410 {errBody = "Course deleted"}
+      | runQuery db (table @"courses" @Schema & restrict (\t -> t ^. col @"id" /= id_ && t ^. col @"name" == name)) & (not . null) ->
+        throwError $ err400 {errBody = "Course " <> show (unName name) <> " already exists"}
+      | otherwise -> do
+        now <- liftIO getCurrentTime
+        let db' =
+              updateTable @"courses" @Schema
+                (\t -> t ^. col @"id" == id_)
+                (\t -> t & col @"name" .~ name & col @"status" .~ status & col @"updatedAt" .~ now)
+                db
+        putDB (db', nextId)
+        pure NoContent
+    _ ->
+      throwError $
+        err500
+          { errBody = "Internal server error, should only have gotten one result with id " <> show id_
+          }
+
+deleteCourse id_ = do
+  (db, nextId) <- getDB
+  case runQuery db (table @"courses" @Schema & restrict (\t -> t ^. col @"id" == id_)) of
+    [] -> throwError $ err404 {errBody = "Course " <> show id_ <> " not found"}
+    [course]
+      | isJust (course ^. col @"deletedAt") -> throwError $ err410 {errBody = "Course deleted"}
+      | otherwise -> do
+        now <- liftIO getCurrentTime
+        let db' =
+              updateTable @"courses" @Schema
+                (\t -> t ^. col @"id" == id_)
+                (\t -> t & col @"updatedAt" .~ now & col @"deletedAt" ?~ now)
+                db
+        putDB (db', nextId)
+        pure NoContent
+    _ ->
+      throwError $
+        err500
+          { errBody = "Internal server error, should only have gotten one result with id " <> show id_
+          }
 
 app :: DB -> Application
 app dbRef = serve courseAPI (hoistServer courseAPI (nt dbRef) server)
