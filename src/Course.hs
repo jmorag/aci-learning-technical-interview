ec8bd02c408e785bf3a671e89d8fea19d2b8a2d9
Joseph Morag <jm@josephmorag.com>
2021-12-19 19:58:39 -0500

    Define Course schema

--- src/Course.hs
+++ src/Course.hs
@@ -1,12 +1,36 @@
-{-# OPTIONS_GHC -fplugin=Evoke #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+
 module Course where
 
-import Data.Time.Clock
 import Data.Aeson
-import Database.Persist
-import Database.Persist.Sqlite
-import Database.Persist.TH
+import Data.Time.Clock
+import Databass
 import Relude
 
 data Status = Scheduled | InProduction | Available
   deriving (Show, Eq)
+
+instance ToJSON Status where
+  toJSON = \case
+    Scheduled -> "scheduled"
+    InProduction -> "in_production"
+    Available -> "available"
+
+instance FromJSON Status where
+  parseJSON = withText "Status" \case
+    "scheduled" -> pure Scheduled
+    "in_production" -> pure InProduction
+    "available" -> pure Available
+    other -> fail "expected one of 'scheduled', 'in_production', or 'available'"
+
+type Course =
+    '[ "id" ::: Int,
+       "name" ::: Text,
+       "status" ::: Status,
+       "createdAt" ::: UTCTime,
+       "updatedAt" ::: UTCTime,
+       "deletedAt" ::: Maybe UTCTime
+     ]
+
+type Schema = '["courses" ::: T Course '["id"]]
